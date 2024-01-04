ca214c61327333ed7a299658d2152533f3c12cc8
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-04 12:08:55 +0700

    lab4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 // App.jsx
-import { useState } from 'react';
-import DogImage from './components/DogImage';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [isShow, setIsShow] = useState(false);
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(false);
+  // useWatch => Tracking State ที่ชื่อ Email
+  useEffect(() => {
+    console.log('Start...validate');
+    if (!email.includes('@')) {
+      // not valid
+      console.log('not valid');
+      setError(true);
+    } else {
+      console.log('valid');
+      setError(false);
+    }
+    console.log('End...validate');
+  }, [email]);
   return (
     <main>
-      {isShow ? <DogImage /> : null}
-      <button onClick={() => setIsShow((cur) => !cur)}>toggle image</button>
+      <input
+        className='border-green-500 border-2 rounded-md w-[300px] focus:outline-none'
+        type='email'
+        style={{ borderColor: error ? 'red' : 'green' }}
+        placeholder='example@mail.com'
+        value={email} // bidning Value with State
+        onChange={(e) => setEmail(e.target.value)} // binding Event with SetState
+      />
     </main>
   );
 }
