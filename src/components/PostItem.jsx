323fb60e8bcd448d83f6853df16eb0454fd00d8a
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-04 14:17:45 +0700

    dynamic fetch by state

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from 'react';
 function PostItem() {
   // https://jsonplaceholder.typicode.com/posts/5
   const [post, setPost] = useState(null);
+  const [postId, setPostId] = useState(1);
 
-  const fetchPostById = () => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/5`)
+  const fetchPostById = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -15,8 +16,8 @@ function PostItem() {
   };
 
   useEffect(() => {
-    fetchPostById();
-  }, []);
+    fetchPostById(postId);
+  }, [postId]);
 
   return (
     <>
@@ -24,10 +25,14 @@ function PostItem() {
         <h1>Loading...</h1>
       ) : (
         <div className='mb-4'>
+          <h1>Post No. {postId}</h1>
           <h1 className='font-bold'>TITLE : {post.title}</h1>
           <p>BODY : {post.body}</p>
         </div>
       )}
+      <button className='btn-primary bg-red-400' onClick={() => setPostId((p) => p + 1)}>
+        next post
+      </button>
     </>
   );
 }
