18fda42daef2ef08cc9b40e2ec536b18cdea79c4
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-04 14:42:38 +0700

    dynimic fetch by props

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import PostItem from './components/PostItem';
 
 function App() {
   const [isShow, setIsShow] = useState(false);
+  const [postId, setPostId] = useState(1);
 
   return (
     <div>
       <button className='btn-primary' onClick={() => setIsShow((p) => !p)}>
         toggle post
       </button>
-      {isShow ? <PostItem /> : null}
+      <button className='btn-primary bg-red-400' onClick={() => setPostId((p) => p + 1)}>
+        next post
+      </button>
+      {isShow ? <PostItem postId={postId} /> : null}
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 
-function PostItem() {
+function PostItem(props) {
   // https://jsonplaceholder.typicode.com/posts/5
   const [post, setPost] = useState(null);
-  const [postId, setPostId] = useState(1);
 
   const fetchPostById = (id) => {
     console.log('START FETCH');
@@ -17,15 +16,8 @@ function PostItem() {
   };
 
   useEffect(() => {
-    let timeOutId = setTimeout(() => {
-      fetchPostById(postId);
-    }, 5000);
-
-    // Cleanup
-    return () => {
-      clearTimeout(timeOutId);
-    };
-  }, [postId]);
+    fetchPostById(props.postId);
+  }, [props.postId]);
 
   return (
     <>
@@ -33,14 +25,11 @@ function PostItem() {
         <h1>Loading...</h1>
       ) : (
         <div className='mb-4'>
-          <h1>Post No. {postId}</h1>
+          <h1>Post No. {props.postId}</h1>
           <h1 className='font-bold'>TITLE : {post.title}</h1>
           <p>BODY : {post.body}</p>
         </div>
       )}
-      <button className='btn-primary bg-red-400' onClick={() => setPostId((p) => p + 1)}>
-        next post
-      </button>
     </>
   );
 }
