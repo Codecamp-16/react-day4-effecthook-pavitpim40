0a130bbd3afb888e9673f1c619d0da794487f658
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-04 13:43:27 +0700

    fetch by click

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // state
+  const [allPosts, setAllPosts] = useState([]);
+
+  // handle Function
+  const fetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {})
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data', data);
+        setAllPosts(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const postRender = allPosts.map((post) => (
+    <div key={post.id} className='mb-4'>
+      <h1 className='font-bold'>TITLE : {post.title}</h1>
+      <p>BODY : {post.body}</p>
+    </div>
+  ));
+
   return (
-    <h1 className='text-3xl italic text-green-500 font-bold underline underline-offset-4'>
-      Hello, React
-    </h1>
+    <div>
+      <button
+        className='p-2 border-2 border-sky-800 rounded-md hover:bg-sky-800 hover:text-white shadow-lg'
+        onClick={fetchData}
+      >
+        fetch Data
+      </button>
+      {allPosts.length > 0 ? postRender : null}
+    </div>
   );
 }
 
