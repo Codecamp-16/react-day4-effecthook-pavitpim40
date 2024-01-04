f0f6b29dbbb89e45d1464567c1b5d7280a14b786
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-04 10:48:58 +0700

    update:demo

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  // state
+  const [count, setCount] = useState(1);
+  const [userInput, setUserInput] = useState('');
+
+  // effect : useEffect(FN,[dependencies])
+
+  // useEffect(() => console.log('## run every render/rerender'));
+  // useEffect(() => {
+  //   // body
+  //   console.log('I am born (didmount == after first render)');
+  //   // return
+  //   return () => {
+  //     console.log('my last word');
+  //   };
+  // }, []);
+  useEffect(() => {
+    // Body
+    console.log('## in useEffect in Count (did mount)', count);
+    // Return
+    return () => {
+      console.log('### cleanup', count);
+    };
+  }, [count]);
+  // useEffect(() => console.log('## in useEffect (input)', userInput), [userInput]);
+  // useEffect(() => console.log('run when count and  userInput change'), [count, userInput]);
+
+  // handle fn ต่าง
+  console.log('# in FC (render/rerender)', count);
   return (
     <div className='text-center'>
       <p>count :{count} </p>
@@ -14,6 +41,7 @@ function Counter() {
       <button className='btn-primary mx-1' onClick={() => setCount((c) => c + 1)}>
         +
       </button>
+      <input type='text' value={userInput} onChange={(e) => setUserInput(e.target.value)} />
     </div>
   );
 }
